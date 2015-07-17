6527cb85646db5122cbe8fb0b1a5db838e4914ca
Jan van Mansum <jan.van.mansum@dans.knaw.nl>
2015-07-17 13:20:31 +0200

    Removed call to "zip" command.
    - Removed log4j-dependency and implemented zipping of a directory in
    scala code. 
    - Zip is still created as temporary file first instead of for example a
    byte array in memory, to avoid problems when the files are very large.
    - MD5 is now calculated during the creation of the temporary zip file.

--- src/main/scala/nl/knaw/dans/easy/archivebag/Main.scala
+++ src/main/scala/nl/knaw/dans/easy/archivebag/Main.scala
@@ -1,73 +1,89 @@
 package nl.knaw.dans.easy.archivebag
 
-import org.rogach.scallop.ScallopConf
-import org.slf4j.LoggerFactory
-import java.net.URL
-import java.io.File
-import net.lingala.zip4j.core.ZipFile
-import net.lingala.zip4j.model.ZipParameters
-import net.lingala.zip4j.util.Zip4jConstants
-import java.net.HttpURLConnection
-import org.apache.commons.io.FileUtils
-import org.apache.commons.codec.binary.Base64
-import java.io.FileInputStream
-import org.apache.commons.codec.digest.DigestUtils
+import java.io.{ByteArrayOutputStream, File, FileInputStream, FileOutputStream, OutputStream}
+import java.math.BigInteger
+import java.nio.file.Paths
+import java.security.{DigestOutputStream, MessageDigest}
+import java.util.zip.{ZipEntry, ZipOutputStream}
+
 import org.apache.commons.io.IOUtils
-import java.io.StringWriter
-import org.apache.http.impl.client.BasicCredentialsProvider
-import org.apache.http.auth.AuthScope
-import org.apache.http.auth.UsernamePasswordCredentials
-import org.apache.http.impl.client.HttpClients
+import org.apache.http.auth.{AuthScope, UsernamePasswordCredentials}
 import org.apache.http.client.methods.HttpPost
-import org.apache.http.entity.FileEntity
-import org.apache.http.entity.ContentType
-import java.io.PrintWriter
-import java.io.OutputStream
-import java.io.ByteArrayOutputStream
-import java.io.BufferedOutputStream
+import org.apache.http.entity.{ContentType, FileEntity}
+import org.apache.http.impl.client.{BasicCredentialsProvider, HttpClients}
+import org.slf4j.LoggerFactory
 
 object Main {
   val log = LoggerFactory.getLogger(getClass)
+  val EASY_BAGIT_URI = "http://easy.dans.knaw.nl/schemas/EASY-BagIt/2015-07-17"
   def main(args: Array[String]) {
     log.debug("Parsing command line arguments")
-    val opts = new Conf(args) 
+    val opts = new Conf(args)
     val bagDir = opts.bagDirectory()
     val username = opts.username()
     val password = opts.password()
     val storageServiceUrl = opts.storageServiceUrl()
 
-    log.info("Creating zipped bag from {}", bagDir)
-    val tempFile = File.createTempFile("easy-archive-bag", ".zip")
-    tempFile.delete() // ... so that zip4j can create it again, yes, it's a hack ...
-    log.debug("Temporary file for zipped bag: {}", tempFile)
-    val p = Runtime.getRuntime.exec(s"zip -r $tempFile ${bagDir.getName}", null, bagDir.getParentFile)
-    if(p.waitFor() != 0) {
-      log.error("Could not create zipped bag. Zip returned: {}", p.exitValue)
-    }
-    val is = new FileInputStream(tempFile)
-    val md5 = DigestUtils.md5Hex(is)
-    is.close()
-    
-    log.info("Sending bag to deposit service ...")
+    log.debug("Generating unique temporary file name")
+    val tempFile = File.createTempFile("easy-archive-bag", "zip")
+    tempFile.delete()
+
+    log.debug("Zipping bag dir to file at {}", tempFile)
+    val fos = new FileOutputStream(tempFile)
+    val md5digest = MessageDigest.getInstance("MD5")
+    compressDirToOutputStream(bagDir, new DigestOutputStream(fos, md5digest))
+
+    val md5hex = new BigInteger(1, md5digest.digest).toString(16)
+    log.debug("Content-MD5 = {}", md5hex)
+
+    log.info("Sending bag to {}, with user = {}, password = {}", storageServiceUrl, username, "*****")
     val credsProv = new BasicCredentialsProvider
-    credsProv.setCredentials(new AuthScope(storageServiceUrl.getHost, storageServiceUrl.getPort), new UsernamePasswordCredentials("USER", "PASSWORD"))
+    credsProv.setCredentials(new AuthScope(storageServiceUrl.getHost, storageServiceUrl.getPort), new UsernamePasswordCredentials(username, password))
     val http = HttpClients.custom.setDefaultCredentialsProvider(credsProv).build()
     val post = new HttpPost(storageServiceUrl.toURI)
     post.addHeader("Content-Disposition", "attachment; filename=bag.zip")
-    post.addHeader("Content-MD5", md5)
-    post.addHeader("Packaging", "http://easy.dans.knaw.nl/schemas/index.xml")
+    post.addHeader("Content-MD5", md5hex)
+    post.addHeader("Packaging", EASY_BAGIT_URI)
     post.setEntity(new FileEntity(tempFile, ContentType.create("application/zip")))
     val response = http.execute(post)
-    if(log.isDebugEnabled) {
+    if (log.isDebugEnabled) {
       val os = new ByteArrayOutputStream()
       IOUtils.copy(response.getEntity.getContent(), os)
       log.debug("Response entity={}", os.toString("UTF-8"))
     }
     response.getStatusLine.getStatusCode match {
-      case 201 => log.info("SUCCESS")
+      case 201 =>
+        log.info("SUCCESS")
         log.info("Deposit created at: {}", response.getFirstHeader("Location").getValue)
       case _ => log.error("Deposit failed: {}", response.getStatusLine)
     }
     tempFile.deleteOnExit()
+  }
+
+  def compressDirToOutputStream(dir: File, os: OutputStream) {
+    val pathBase = Paths.get(dir.getParentFile.getAbsolutePath)
+    val zos = new ZipOutputStream(os)
+    def writeFile(f: File) = {
+      val fis = new FileInputStream(f)
+      IOUtils.copy(fis, zos)
+      fis.close()
+    }
+    zos.putNextEntry(new ZipEntry(dir.getName + "/"))
+    recursiveListFiles(dir).map {
+      case f: File =>
+        val relativePath = pathBase.relativize(Paths.get(f.getAbsolutePath))
+        val pathString = relativePath + 
+            (if (f.isDirectory()) "/" 
+             else "")
+        zos.putNextEntry(new ZipEntry(pathString))
+        if (f.isFile) writeFile(f)
+        zos.closeEntry()
+    }
+    zos.close()
+  }
+
+  def recursiveListFiles(f: File): Array[File] = {
+    val fs = f.listFiles
+    fs ++ fs.filter(_.isDirectory).flatMap(recursiveListFiles)
   }
 }
