9c56278388751dd4975ed90b8c560f050724fdf2
pfrolke <52530667+pfrolke@users.noreply.github.com>
2019-12-02 14:21:55 +0100

    EASY-2433 Convert to maven multi-module (#63)
    Converted to multi-module maven project.

---
+++ command/src/main/scala/nl.knaw.dans.easy.archivebag.command/CommandLineOptions.scala
@@ -0,0 +1,112 @@
+/**
+ * Copyright (C) 2015 DANS - Data Archiving and Networked Services (info@dans.knaw.nl)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package nl.knaw.dans.easy.archivebag.command
+
+import java.io.File
+import java.net.{ MalformedURLException, URI, URL }
+import java.nio.file.Paths
+import java.util.UUID
+
+import nl.knaw.dans.lib.logging.DebugEnhancedLogging
+import org.rogach.scallop.{ ScallopConf, ScallopOption, ValueConverter, singleArgConverter }
+
+import nl.knaw.dans.easy.archivebag.Parameters
+
+object CommandLineOptions extends DebugEnhancedLogging {
+  def parse(args: Array[String]): Parameters = {
+    debug("Loading application.properties ...")
+
+    val configuration = Configuration(Paths.get(System.getProperty("app.home")))
+    debug("Parsing command line ...")
+    val cmd = new ScallopCommandLine(configuration, args)
+    cmd.verify()
+
+    val settings = Parameters(
+      username = cmd.username(),
+      password = cmd.password(),
+      bagDir = cmd.bagDirectory(),
+      tempDir = new File(configuration.properties.getString("tempdir")),
+      storageDepositService = cmd.bagStoreUrl(),
+      bagIndexService = new URI(configuration.properties.getString("bag-index.uri")),
+      bagId = UUID.fromString(cmd.uuid()),
+      userAgent = s"easy-archive-bag/${configuration.version}",
+    )
+
+    debug(s"Using the following settings: $settings")
+
+    settings
+  }
+}
+
+class ScallopCommandLine(configuration: Configuration, args: Array[String]) extends ScallopConf(args) {
+
+  private implicit val urlConverter: ValueConverter[URL] = singleArgConverter(s => new URL(addTrailingSlashIfNeeded(s)), {
+    case e: MalformedURLException => Left(s"bad URL, ${ e.getMessage }")
+  })
+
+  private def addTrailingSlashIfNeeded(s: String): String = {
+    if (s endsWith "/") s
+    else s"$s/"
+  }
+
+  appendDefaultToDescription = true
+  editBuilder(_.setHelpWidth(110))
+
+  printedName = "easy-archive-bag"
+  version(s"$printedName ${ configuration.version }")
+  banner(
+    s"""
+       |Send a bag to archival storage.
+       |
+       |Usage: $printedName <bag-directory> <uuid> [<storage-service-url>]
+       |Options:
+       |""".stripMargin)
+
+  val username: ScallopOption[String] = opt[String](
+    name = "username",
+    short = 'u',
+    descr = "Username to use for authentication/authorisation to the storage service",
+    required = true,
+  )
+
+  val password: ScallopOption[String] = opt[String](
+    name = "password",
+    short = 'p',
+    descr = "Password to use for authentication/authorisation to the storage service",
+    required = true,
+  )
+
+  val bagDirectory: ScallopOption[File] = trailArg[File](
+    name = "bag-directory",
+    descr = "Directory in BagIt format that will be sent to archival storage",
+  )
+
+  val uuid: ScallopOption[String] = trailArg[String](
+    name = "uuid",
+    descr = "Identifier for the bag in archival storage",
+  )
+
+  val bagStoreUrl: ScallopOption[URL] = trailArg[URL](
+    name = "bag-store-url",
+    descr = "base url to the store in which the bag needs to be archived",
+  )
+
+  validateFileExists(bagDirectory)
+  validateOpt(bagDirectory) {
+    case Some(_) => Right(Unit)
+    case _ => Left("Could not parse parameter <bag-directory>")
+  }
+}
---
+++ lib/src/main/scala/nl.knaw.dans.easy.archivebag/EasyArchiveBag.scala
@@ -0,0 +1,162 @@
+/**
+ * Copyright (C) 2015 DANS - Data Archiving and Networked Services (info@dans.knaw.nl)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package nl.knaw.dans.easy.archivebag
+
+import java.io.{ File, IOException }
+import java.net.URI
+import java.nio.charset.StandardCharsets
+import java.nio.file.Path
+
+import net.lingala.zip4j.core.ZipFile
+import net.lingala.zip4j.model.ZipParameters
+import nl.knaw.dans.lib.error._
+import nl.knaw.dans.lib.logging.DebugEnhancedLogging
+import nl.knaw.dans.lib.string._
+import org.apache.commons.codec.digest.DigestUtils
+import org.apache.commons.io.FileUtils
+import resource.Using
+import scalaj.http.HttpResponse
+
+import scala.util.control.NonFatal
+import scala.util.{ Failure, Success, Try }
+
+object EasyArchiveBag extends Bagit5FacadeComponent with DebugEnhancedLogging {
+
+  override val bagFacade = new Bagit5Facade()
+
+  def run(implicit ps: Parameters): Try[URI] = {
+    logger.info(s"[${ps.bagId}] Archiving bag")
+    for {
+      maybeVersionOfId <- bagFacade.getIsVersionOf(ps.bagDir.toPath)
+      _ <- maybeVersionOfId.map(createRefBagsTxt).getOrElse(Success(()))
+      zippedBag <- generateUncreatedTempFile()
+      _ <- zipDir(ps.bagDir, zippedBag)
+      response <- putFile(zippedBag)
+      location <- handleBagStoreResponse(response)
+        .doIfSuccess(_ => zippedBag.delete())
+        .doIfFailure { case _ => zippedBag.delete() }
+    } yield location
+  }
+
+  private def handleBagStoreResponse(response: HttpResponse[String])(implicit ps: Parameters): Try[URI] = {
+    response.code match {
+      case 201 =>
+        response.location
+          .map(loc => Success(new URI(loc)))
+          .getOrElse(Failure(new RuntimeException("No bag archival location found in response after successful upload")))
+          .doIfSuccess(uri => logger.info(s"Bag archival location created at: $uri"))
+          .flatMap(location => addBagToIndex(ps.bagId)
+            .map(_ => location)
+            .recover {
+              case t =>
+                logger.warn(s"BAG ${ ps.bagId } NOT ADDED TO BAG-INDEX. SUBSEQUENT REVISIONS WILL NOT BE PRUNED", t)
+                location
+            })
+      case 401 =>
+        Failure(UnautherizedException(ps.bagId))
+      case _ =>
+        logger.error(s"${ ps.storageDepositService } returned:[ ${ response.statusLine } ]. Body = ${ response.body }")
+        Failure(new RuntimeException(s"Bag archiving failed: ${ response.statusLine }"))
+    }
+  }
+
+  private def addBagToIndex(bagId: BagId)(implicit ps: Parameters): Try[Unit] = Try {
+    val response = ps.http(ps.bagIndexService.resolve(s"bags/$bagId").toASCIIString)
+      .method("PUT")
+      .asString
+
+    response.code match {
+      case 201 => ()
+      case _ =>
+        logger.error(s"${ ps.bagIndexService } returned:[ ${ response.statusLine } ] while adding new bag to bag index. Body = ${ response.body }")
+        throw new IllegalStateException("Error trying to add bag to index")
+    }
+  }
+
+  private def createRefBagsTxt(versionOfId: BagId)(implicit ps: Parameters): Try[Unit] = {
+    for {
+      refBagsTxt <- getBagSequence(versionOfId)
+      _ <- writeRefBagsTxt(ps.bagDir.toPath)(refBagsTxt)
+    } yield ()
+  }
+
+  private def getBagSequence(bagId: BagId)(implicit ps: Parameters): Try[String] = Try {
+    val uri = ps.bagIndexService.resolve(s"bag-sequence?contains=$bagId")
+    val response = ps.http(uri.toASCIIString)
+      .header("Accept", "text/plain;charset=utf-8")
+      .asString
+
+    response.code match {
+      case 200 =>
+        val body = response.body
+        if (body.isBlank) {
+          logger.error(s"Empty response body from [$uri]")
+          throw InvalidIsVersionOfException(s"Bag with bag-id $bagId, pointed to by Is-Version-Of field in bag-info.txt is not found in bag index.")
+        }
+        else body
+      case _ =>
+        logger.error(s"$uri returned: [ ${ response.statusLine } ] while getting bag Sequence for bag $bagId. Body = ${ response.body }")
+        throw new IllegalStateException(s"Error retrieving bag-sequence for bag: $bagId. [$uri] returned ${ response.statusLine }")
+    }
+  }
+
+  private def writeRefBagsTxt(bagDir: Path)(refBagsTxt: String): Try[Unit] = Try {
+    FileUtils.write(bagDir.resolve("refbags.txt").toFile, refBagsTxt, "UTF-8")
+  }
+
+  private def generateUncreatedTempFile()(implicit tempDir: File): Try[File] = Try {
+    val tempFile = File.createTempFile("easy-archive-bag-", ".zip", tempDir)
+    tempFile.delete()
+    debug(s"Generated unique temporary file name: $tempFile")
+    tempFile
+  } recoverWith {
+    case NonFatal(e) => Failure(new IOException(s"Could not create temp file in $tempDir: ${ e.getMessage }", e))
+  }
+
+  private def zipDir(dir: File, zip: File)(implicit bagId: BagId): Try[Unit] = Try {
+    logger.info(s"[$bagId] Zipping directory $dir to file $zip")
+    if (zip.exists) zip.delete
+    val zf = new ZipFile(zip) {
+      setFileNameCharset(StandardCharsets.UTF_8.name)
+    }
+    val parameters = new ZipParameters
+    zf.addFolder(dir, parameters)
+  }
+
+  private def putFile(file: File)(implicit s: Parameters): Try[HttpResponse[String]] = {
+    for {
+      md5Hex <- computeMd5(file)
+      _ = debug(s"Content-MD5 = $md5Hex")
+      _ = logger.info(s"Sending bag to ${ s.storageDepositService }, id = ${ s.bagId }, with user = ${ s.username }, password = ****")
+      response <- Using.fileInputStream(file)
+        .map(fileStream => {
+          s.http(s.storageDepositService.toURI.resolve("bags/").resolve(s.bagId.toString).toASCIIString)
+            .copy(connectFunc = InputStreamBodyConnectFunc(fileStream, Option(file.length)))
+            .header("Content-Disposition", "attachment; filename=bag.zip")
+            .header("Content-MD5", md5Hex)
+            .header("Content-Type", "application/zip")
+            .method("PUT")
+            .auth(s.username, s.password)
+            .asString
+        })
+        .tried
+    } yield response
+  }
+
+  private def computeMd5(file: File): Try[String] = {
+    Using.fileInputStream(file).map(DigestUtils.md5Hex).tried
+  }
+}
